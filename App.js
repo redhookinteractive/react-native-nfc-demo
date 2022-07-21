95b97418ef8c7379ab18ce15b8c6b66dd06b23dd
Dustin Rea <drea08@gmail.com>
2022-07-21 12:00:28 -0400

    initial commit

---
+++ App.js
@@ -0,0 +1,69 @@
+import { Provider } from 'react-redux';
+import React from 'react';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  ActivityIndicator,
+  StyleSheet,
+} from 'react-native';
+import { PersistGate } from 'redux-persist/integration/react';
+import { NavigationContainer } from '@react-navigation/native';
+import { colors } from './src/styles';
+
+import { store, persistor } from './src/redux/store';
+
+import AppView from './src/modules/AppViewContainer';
+
+import NfcManager, { NfcTech } from 'react-native-nfc-manager';
+
+// Pre-step, call this before any NFC operations
+NfcManager.start();
+
+export default function App() {
+  async function readNdef() {
+    try {
+      // register for the NFC tag with NDEF in it
+      await NfcManager.requestTechnology(NfcTech.Ndef);
+      // the resolved tag object will contain `ndefMessage` property
+      const tag = await NfcManager.getTag();
+      console.warn('SUCCESS: Tag found', tag);
+    } catch (ex) {
+      console.warn('ERROR: Oops!', ex);
+      console.log(ex);
+    } finally {
+      // stop the nfc scanning
+      NfcManager.cancelTechnologyRequest();
+    }
+  }
+
+  return (
+    <Provider store={store}>
+      <NavigationContainer>
+        <PersistGate
+          loading={
+            // eslint-disable-next-line react/jsx-wrap-multilines
+            <View style={styles.container}>
+              <ActivityIndicator color={colors.red} />
+            </View>
+          }
+          persistor={persistor}
+        >
+          <AppView />
+          <TouchableOpacity onPress={readNdef}>
+            <Text>Scan a Tag</Text>
+          </TouchableOpacity>
+        </PersistGate>
+      </NavigationContainer>
+    </Provider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'white',
+  },
+});
